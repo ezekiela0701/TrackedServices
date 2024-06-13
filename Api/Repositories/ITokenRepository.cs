53045448a170fcdb7f3f40b56a2d185a0a25b255
Ezekiela <Ezekielaanderson@gmail.com>
2024-06-13 09:34:38 +0200

    Token Autocreation finish | loading inject token into services

---
+++ Api/Repositories/ITokenRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity ; 
+
+
+
+namespace Api.Repositories
+{
+    public interface ITokenRepository
+    {
+
+        string CreateJwtToken(IdentityUser user , List<string> roles) ;
+
+    }
+}
